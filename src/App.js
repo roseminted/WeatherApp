f963ac25426e70c815b5386ff8c06bc5cac73dab
roseminted <57846775+roseminted@users.noreply.github.com>
2020-12-18 14:35:11 -0500

    add openweather api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const { REACT_APP_OPENWEATHER_KEY, REACT_APP_OPENWEATHER_BASEURL } = process.env;
 
 function App() {
-  return (
-    <div className="App">
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
 
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${REACT_APP_OPENWEATHER_BASEURL}weather?q=${query}&units=imperial&APPID=${REACT_APP_OPENWEATHER_KEY}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Seach..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}>
+          </input>
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">
+            32°f
+          </div>
+          <div className="weather">
+            Sunny
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
